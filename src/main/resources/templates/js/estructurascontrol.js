e16a1b5daccdfed4d41d86a4fa39902ab6c00612
Gofra2003 <121211543+Gofra2003@users.noreply.github.com>
2024-07-19 21:10:49 -0500

    estructuras de control html css y js
    pagina con la informacion y video de las estructuras, agreguen ustedes la ruta del video

---
+++ src/main/resources/templates/js/estructurascontrol.js
@@ -0,0 +1,9 @@
+function navigateToNextPage() {
+    const container = document.querySelector('.container');
+    container.style.transition = 'transform 0.5s ease-in-out';
+    container.style.transform = 'translateX(-100vw)';
+    
+    setTimeout(() => {
+        window.location.href = 'src/main/resources/templates/nextpage.html'; // Cambia 'nextpage.html' a la URL de la siguiente página
+    }, 500);
+}
