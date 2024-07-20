52c2733b742cd6d83db7261abfb9710376071b3f
Gofra2003 <121211543+Gofra2003@users.noreply.github.com>
2024-07-19 22:49:19 -0500

    html css y js  de armar codigo completo

---
+++ src/main/resources/templates/js/armarC.js
@@ -0,0 +1,53 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    if (event.target.classList.contains('correct')) {
+        event.preventDefault(); // No permite arrastrar si ya está correctamente colocado
+    } else {
+        event.dataTransfer.setData("text", event.target.id);
+    }
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var droppedElement = document.getElementById(data);
+    var dropZone = event.target;
+
+    // Check if the dropZone is already correct
+    if (dropZone.classList.contains('correct')) {
+        return; // No permite reemplazar una zona correcta
+    }
+
+    // Check if the dropZone is a valid drop target
+    if (dropZone.classList.contains('drop-zone') && dropZone.textContent.trim() === "") {
+        dropZone.textContent = droppedElement.textContent.trim();
+        droppedElement.style.display = "none";
+
+        // Check if the dropped answer is correct
+        if (dropZone.textContent.trim() === dropZone.getAttribute("data-correct").trim()) {
+            dropZone.style.backgroundColor = "lightgreen";
+            dropZone.classList.add('correct');
+            droppedElement.classList.add('correct');
+        } else {
+            dropZone.style.backgroundColor = "#ffbfaa";
+            setTimeout(() => {
+                dropZone.textContent = "";
+                dropZone.style.backgroundColor = "";
+                droppedElement.style.display = "inline-block";
+            }, 1000);
+        }
+    }
+}
+
+function navigateToNextPage() {
+    const container = document.querySelector('.container');
+    container.style.transition = 'transform 0.5s ease-in-out';
+    container.style.transform = 'translateX(-100vw)';
+
+    setTimeout(() => {
+        window.location.href = 'src/main/resources/templates/nextpage.html'; // Cambia 'nextpage.html' a la URL de la siguiente página
+    }, 500);
+}
